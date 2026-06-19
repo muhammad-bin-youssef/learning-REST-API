fa5124399d9d8e9c6cef34c057286462be18ff9e
muhammad-bin-youssef <mhbyn@proton.me>
2026-06-19 21:39:00 +0300

    Semiautomatic git commit

--- project/profile_api/views.py
+++ project/profile_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        an_apiview = [
+            "H1",
+        ]
+        return Response({"message": "hello", "an_apiview": an_apiview})
