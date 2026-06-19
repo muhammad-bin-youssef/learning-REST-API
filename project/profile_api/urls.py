8cecb435a01cc7764566328867c7c64fbb71df2d
muhammad-bin-youssef <mhbyn@proton.me>
2026-06-20 00:13:59 +0300

    Semiautomatic git commit

--- project/profile_api/urls.py
+++ project/profile_api/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import EchoView, HelloApiView
+from .views import EchoView, HelloApiView, HelloViewSet
+
+router = DefaultRouter()
+router.register("vs", HelloViewSet, base_name="hello-viewset")
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("", HelloApiView.as_view()),
     path("echo/", EchoView.as_view()),
 ]
--- project/profile_api/views.py
+++ project/profile_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView, status
+from rest_framework.viewsets import ViewSet
 
 from .serializers import EchoSerializer, HelloSerializer
 
@@ -32,6 +33,40 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({"message": "DELETE"})
+
+
+class HelloViewSet(ViewSet):
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        return Response({"message": "hello"})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}!"
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+
+        return Response({"http_method": "GET"})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({"http_method": "PUT"})
+
+    def partial_update(self, request, pk=None):
+
+        return Response({"http_method": "PATCH"})
+
+    def destroy(self, request, pk=None):
+
+        return Response({"http_method": "DELETE"})
 
 
 class EchoView(APIView):
