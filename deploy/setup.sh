5d0c1bed45696fba0810bf24502122db8c8a0a25
MHY <232122495+muhammad-bin-youssef@users.noreply.github.com>
2026-06-27 18:29:44 +0300

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -7,20 +7,33 @@ PROJECT_GIT_URL='https://github.com/muhammad-bin-youssef/learning-REST-API.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-# Set Ubuntu Language
+# Set Ubuntu Language non-interactively
+export DEBIAN_FRONTEND=noninteractive
 locale-gen en_GB.UTF-8
 
 # Install Python, SQLite3 and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git build-essential
 
 mkdir -p $PROJECT_BASE_PATH
+# Delete directory if it already exists to prevent git clone failure
+if [ -d "$PROJECT_BASE_PATH/.git" ]; then
+    rm -rf "$PROJECT_BASE_PATH"
+fi
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 python3 -m venv $PROJECT_BASE_PATH/env
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Fix: Inject compiler flag to ignore incompatible pointer errors during uWSGI build
+export CFLAGS="-Wno-error=incompatible-pointer-types"
+
+$PROJECT_BASE_PATH/env/bin/pip install --upgrade pip setuptools wheel
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.21
+
+# Clear compiler flags after execution
+unset CFLAGS
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
@@ -33,7 +46,8 @@ supervisorctl restart profiles_api
 
 # Setup nginx to make our application accessible.
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
+rm -f /etc/nginx/sites-enabled/default
+rm -f /etc/nginx/sites-enabled/profiles_api.conf
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
 
