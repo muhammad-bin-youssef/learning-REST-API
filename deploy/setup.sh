419623337e1caec189460dec480c68822a2994ce
MHY <232122495+muhammad-bin-youssef@users.noreply.github.com>
2026-06-27 18:21:23 +0300

    Update comment for SQLite to SQLite3

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,7 +10,7 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
+# Install Python, SQLite3 and pip
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
